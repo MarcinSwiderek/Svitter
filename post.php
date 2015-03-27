b606b1d15b7163e520d8570f28d5de82dfecf5ab
Marcin Swiderek <svdr23@gmail.com>
2015-03-27 14:53:59 +0100

    Działa dodawanie komentarzy do bazy, jeszcze nie ma wyswietlania

--- post.php
+++ post.php
@@ -3,7 +3,17 @@ include("header.php");
 ?>
 <main>
 	<?php 
-	if($_SERVER['REQUEST_METHOD']==="GET") {
+		
+		if($_SERVER['REQUEST_METHOD']==="POST") {
+			if(isset($_POST['comment_input']) && strlen($_POST['comment_input']) > 0 ) {
+				$commentdate=date('Y-m-d H:i:s');
+				$sql='INSERT INTO Comments(user_id, post_id, comment_text, comment_date) VALUES("'.$_SESSION['user_id'].'",
+						"'.$_POST['post_id'].'","'.$_POST['comment_input'].'","'.$commentdate.'")';
+				$result=$conn->query($sql);
+				
+			}
+		}
+		
 		$post=$_GET['page_id'];
 		$sql='SELECT * FROM Users JOIN Posts on Users.id=Posts.user_id WHERE Posts.post_id="'.$_GET['page_id'].'"';
 		$result=$conn->query($sql);
@@ -26,20 +36,23 @@ include("header.php");
 								<legend>Dodaj komentarz</legend>
 								<form method='POST' name='comment_form' action='#'>
 									<textarea rows='3' cols='30' name='comment_input' maxlength='60'></textarea>
-									<button type='submit' name='comment_submit_button'>Wyślij</button>	  
+									<button type='submit' name='comment_submit_button'>Wyślij</button>
+									<input type='hidden' value='{$post}' name='post_id'>	  
 								</form>
 							</fieldset>
 							
 						");
+					//tutaj wyświetlanie komentarzy do posta
+					echo ("<h2>Najnowsze komentarze: </h2>");
 			}
 		} 
 		else {
 			"Nie ma takiego postu.";
 		}
-	}
-	else {
-		echo ("błąd");
-	}
+		
+	
+	
+	
 	?>
 
 
